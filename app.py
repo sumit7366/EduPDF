61edb65461e2d624178f3a4d05bfe727f325cff3
Sumit Kumar <120784270+sumit7366@users.noreply.github.com>
2025-10-23 03:00:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1142,4 +1142,4 @@ if __name__ == '__main__':
     print("=" * 60)
     
     # For production, use this:
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5001)
